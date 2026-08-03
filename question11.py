5121f1fef7011b8432aeaa9ea24e14e5bd020bb0
Anshu Rajput <anshurajput312@outlook.com>
2026-08-03 15:47:37 +0530

    practice

--- question11.py
+++ question11.py
@@ -1,7 +1,1 @@
-# WRP to find the number of digits in given number
-n = int(input("Enter a number: "))
-count = 0
-while n > 0:
-    count += 1
-    n //= 10
-print("Number of digits:", count) 
+# Write 
