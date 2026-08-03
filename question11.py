2b651e306453881a4cdda7fd910a982476e6ba23
Anshu Rajput <anshurajput312@outlook.com>
2026-08-03 15:48:12 +0530

    practice

--- question11.py
+++ question11.py
@@ -1,1 +1,7 @@
-# Write 
+# write a program to find the number of digits in a given number.
+number = int(input("Enter a number: "))
+count = 0
+while number != 0:
+    number //= 10
+    count += 1
+print("The number of digits in the given number is:", count)
---
+++ question12.py
@@ -0,0 +1,49 @@
+# Write a program to generate a menu which will have options of 
+# 1. sum of n natural numbers 
+# 2. reverse of given number 
+# 3. Count how many times a input number is repeated
+# 4. Lcm of two digits
+# 5. exit
+# ask value form user
+while True:
+    print("1. Sum of n natural numbers")
+    print("2. Reverse of given number")
+    print("3. Count how many times a input number is repeated")
+    print("4. Lcm of two digits")
+    print("5. Exit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        n = int(input("Enter a number: "))
+        sum_n = n * (n + 1) // 2
+        print("The sum of first", n, "natural numbers is:", sum_n)
+
+    elif choice == 2:
+        number = int(input("Enter a number: "))
+        reverse = 0
+        while number != 0:
+            digit = number % 10
+            reverse = reverse * 10 + digit
+            number //= 10
+        print("The reverse of the given number is:", reverse)
+
+    elif choice == 3:
+        num = int(input("Enter a number: "))
+        count = 0
+        for i in range(1, num + 1):
+            if i == num:
+                count += 1
+        print("The number", num, "is repeated", count, "times.")
+
+    elif choice == 4:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        lcm = (num1 * num2) // gcd(num1, num2)
+        print("The LCM of", num1, "and", num2, "is:", lcm)
+
+    elif choice == 5:
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
